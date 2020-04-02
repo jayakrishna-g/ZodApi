eab4d49eff59616c89a2eae89e0a8835fc403f29
jayakrishna-g <gandhamallajayakrishna@hotmail.com>
2020-04-02 12:18:46 +0530

    Enabled CORS

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const express = require('express')
 const config = require('./db/config')
 const playerLib = require('./db/lib/player')
 const db = require('./db/connect')
+const cors = require('cors')
 
 const app = express()
-
+app.use(cors())
 
 db.connect()
 
